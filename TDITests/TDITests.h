9bcb25dc936f205fd45975329e3692d6b5442f1a
pepibumur <pepibumur@gmail.com>
2013-06-13 10:42:49 +0200

    Primera subida del proyecto

---
+++ TDITests/TDITests.h
@@ -0,0 +1,13 @@
+//
+//  TDITests.h
+//  TDITests
+//
+//  Created by Pedro Piñera Buendía on 17/04/13.
+//  Copyright (c) 2013 PPinera. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TDITests : SenTestCase
+
+@end
---
+++ TDITests/TDITests.m
@@ -0,0 +1,32 @@
+//
+//  TDITests.m
+//  TDITests
+//
+//  Created by Pedro Piñera Buendía on 17/04/13.
+//  Copyright (c) 2013 PPinera. All rights reserved.
+//
+
+#import "TDITests.h"
+
+@implementation TDITests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TDITests");
+}
+
+@end
